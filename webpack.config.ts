14e7faaf4c96742bf5faeffb02e81034fae527b1
Jhon Casallas <jhoncbernal@gmail.com>
2022-02-12 18:23:05 -0500

    style: webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,70 +1,70 @@
-import path from 'path';
-import webpack from 'webpack';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import nodeExternals from 'webpack-node-externals';
-import NodemonPlugin from 'nodemon-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
-import ESLintPlugin from 'eslint-webpack-plugin';
-import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
-import dotenv from 'dotenv';
-import fs from 'fs';
+import path from "path";
+import webpack from "webpack";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import nodeExternals from "webpack-node-externals";
+import NodemonPlugin from "nodemon-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
+import ESLintPlugin from "eslint-webpack-plugin";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import dotenv from "dotenv";
+import fs from "fs";
 
-const { NODE_ENV = 'production' }: any = process.env;
+const { NODE_ENV = "production" }: any = process.env;
 
-const copyEnv: any = fs.existsSync('./.env')
+const copyEnv: any = fs.existsSync("./.env")
   ? [
       new CopyPlugin({
-        patterns: [{ from: '.env' }]
-      })
+        patterns: [{ from: ".env" }],
+      }),
     ]
   : [];
 
 const config: webpack.Configuration = {
-  entry: [path.resolve(__dirname, 'src')],
+  entry: [path.resolve(__dirname, "src")],
   mode: NODE_ENV,
-  watch: NODE_ENV === 'development',
-  target: 'node',
+  watch: NODE_ENV === "development",
+  target: "node",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.js",
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    plugins: [new TsconfigPathsPlugin()]
+    extensions: [".tsx", ".ts", ".js"],
+    plugins: [new TsconfigPathsPlugin()],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.hbs?$/,
-        use: 'handlebars-loader'
-      }
-    ]
+        use: "handlebars-loader",
+      },
+    ],
   },
-  devtool: 'source-map',
-  externals: [nodeExternals()] as webpack.Configuration['externals'],
+  devtool: "source-map",
+  externals: [nodeExternals()] as webpack.Configuration["externals"],
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
-      'process.env': JSON.stringify(dotenv.config().parsed)
+      "process.env": JSON.stringify(dotenv.config().parsed),
     }),
     new ESLintPlugin({
-      extensions: ['.tsx', '.ts', '.js']
+      extensions: [".tsx", ".ts", ".js"],
     }),
     new NodemonPlugin({
-      ignore: ['./node_modules'],
+      ignore: ["./node_modules"],
       verbose: true,
-      delay: 500
+      delay: 500,
     }),
-    ...copyEnv
-  ] as webpack.Configuration['plugins'],
+    ...copyEnv,
+  ] as webpack.Configuration["plugins"],
   stats: {
-    warnings: true
-  }
+    warnings: true,
+  },
 };
 
 export default config;
